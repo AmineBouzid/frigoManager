20aaa4e54eb297d69801ec480dfcf10c3be21281
BAYON <bayonguillaume43@gmail.com>
2020-11-18 16:34:08 +0100

    Modifying the pantry interface

--- frigomanager/src/main/java/tse/crewmatse/frigomanager/controller/PantryController.java
+++ frigomanager/src/main/java/tse/crewmatse/frigomanager/controller/PantryController.java
@@ -1,10 +1,16 @@
 package tse.crewmatse.frigomanager.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
 import tse.crewmatse.frigomanager.App;
 
-public class PantryController {
+public class PantryController implements Initializable {
+	@FXML private SplitPane pantrySplitPane;
 
     @FXML
     private void switchToProfile() throws IOException {
@@ -30,5 +36,17 @@ public class PantryController {
     private void switchToAlerts() throws IOException {
         App.setRoot("alerts");
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		pantrySplitPane.setDividerPositions(0.8f);
+		//pantrySplitPane.setDisable(true);
+		
+	}
+    
+   
+    
+    
     
 }
